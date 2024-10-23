c52affcdd25b0bfb1257532f7c4e501ffe818e62
Yian Kim <devyiankim@gmail.com>
2024-10-24 03:06:56 +0900

    [#5] feat: deploy with gh-pages branch

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio/',
   experimental: {
     // appDir: true,
   },
